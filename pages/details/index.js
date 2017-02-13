9328357d5615e0d866715a9a9337207fdde8b96e
Jay <870301@qq.com>
2017-02-13 21:02:59 +0800

    1
    1

---
+++ pages/details/index.js
@@ -0,0 +1,43 @@
+var app = getApp()
+Page({
+    data: {
+        indicatorDots: true,
+        vertical: false,
+        autoplay: true,
+        interval: 3000,
+        duration: 1200,
+    },
+
+    onLoad: function(options) {
+
+        var that = this
+        
+        // 商品详情
+        wx.request({
+            url: 'http://huanqiuxiaozhen.com/wemall/goods/inqgoods?id=' + options.id,
+            method: 'GET',
+            data: {},
+            header: {
+                'Accept': 'application/json'
+            },
+            success: function(res) {
+
+                var goodsPicsInfo = [];
+                var goodsPicsObj = {};
+                var goodspic = res.data.data.goodspics;
+                var goodspics = goodspic.substring(0, goodspic.length - 1);
+                var goodspicsArr = goodspics.split("#");
+                for (var i = 0; i < goodspicsArr.length; i++) {
+                    goodsPicsInfo.push({
+                        "picurl": goodspicsArr[i]
+                    });
+                }
+                that.setData({
+                    shopppingDetails: res.data.data,
+                    goodsPicsInfo: goodsPicsInfo
+                })
+            }
+        })
+
+    }
+})
